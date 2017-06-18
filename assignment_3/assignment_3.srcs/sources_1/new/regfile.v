ae6f76777cffb9066d2942f1c73c6b8e5042ba31
Willster419 <willster419@outlook.com>
2017-06-18 00:22:55 -0400

    updated assignment_3. fixed gitignore files

--- assignment_3/assignment_3.srcs/sources_1/new/regfile.v
+++ assignment_3/assignment_3.srcs/sources_1/new/regfile.v
@@ -7,21 +7,22 @@ module regfile(
   input [31:0] dbus,//data in
   output [31:0] abus,//data out
   output [31:0] bbus,//data out
-  wire [31:0] abusW,
-  wire [31:0] bbusW,
   input clk
   );
+  //wire [31:0] abusW;
+  //wire [31:0] bbusW;
   //assign abus = abusW;
   //assign bbus = bbusW;
-  assign abus = Aselect[0] ? 32'b0 : abusW;//in case it's the 0
-  assign bbus = Bselect[0] ? 32'b0 : bbusW;
-  DNegflipFlop myFlips[31:0](//32 wide register
+  assign abus = Aselect[0] ? 32'b0 : 32'bz;//in case it's the 0
+  assign bbus = Bselect[0] ? 32'b0 : 32'bz;
+  DNegflipFlop myFlips[30:0](//32 wide register
       .dbus(dbus),
-      .abus(abusW),
-      .Dselect(Dselect),
-      .Bselect(Bselect),
-      .Aselect(Aselect),
-      .bbus(bbusW)
+      .abus(abus),
+      .Dselect(Dselect[31:1]),
+      .Bselect(Bselect[31:1]),
+      .Aselect(Aselect[31:1]),
+      .bbus(bbus),
+      .clk(clk)
     );
     /*assign abus = Aselect[0] ? 32'b0 : 32'bz;//in case it's the 0
     assign bbus = Bselect[0] ? 32'b0 : 32'bz;*/
@@ -33,16 +34,19 @@ module regfile(
     end*/
   endmodule
   //negedge sensitive flipflop module flip flop module
-  module DNegflipFlop(dbus, abus, Dselect, Bselect, Aselect, bbus);
+  module DNegflipFlop(dbus, abus, Dselect, Bselect, Aselect, bbus, clk);
     input [31:0] dbus;
     input Dselect;//the select write bit for this register
     input Bselect;//the select read bit for this register
     input Aselect;
+    input clk;
     output [31:0] abus;
     output [31:0] bbus;
     reg [31:0] data;
-    always @(posedge Dselect) begin
+    always @(negedge clk) begin
+      if(Dselect) begin
       data = dbus;
+      end
     end
     assign abus = Aselect ? data : 32'bz;
     assign bbus = Bselect ? data : 32'bz;
